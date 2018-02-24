7d2c03b31be2d418fd4f882dfbeee90467d1ee80
Calliari <jrcalliari@me.com>
2018-02-24 00:46:17 +0000

    commands to mount s3fs on AWS ec2 instance with s3 bucket

---
+++ s3fs/script_mount_s3fs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+
+# # mount a s3fs on a ec2, notes:
+mkdir /new_directory
+chmod 777 new_directory
+
+# echo "accessKeyId:secretAccessKey" > ~/.passwd-s3fs
+# sudo chmod 600 ~/.passwd-s3fs
+
+# The s3 user with the "accessKeyId" credentioal above must have the correct permissions to mount s3fs
+sudo s3fs S3_bucket -o use_cache=/tmp  /new_directory -o allow_other,uid=33,gid=33,umask=007
+
