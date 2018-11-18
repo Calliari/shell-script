394bb8a4931796e4e2317550ea02e00b03dd6d8a
calliari <jrcalliari@me.com>
2018-11-18 10:11:05 +0000

    update and add few monit initial setup

---
+++ monit_setup/monit_setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# instructions on how to install, send email and restart service with monit 
+
+# update the server
+sudo apt-get update -y
+
+
+# install monit on ubuntu
+sudo apt-get install monit -y
+
+
+#send email when trigger the conditional event
+
+
+
+
+# restart a service when the condition is achievided
+
+
