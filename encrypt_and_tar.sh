8b6e05d0995ae9fc99b4cecbfeb4be8b20163e26
Caliari Jr <Calliari@users.noreply.github.com>
2019-05-28 15:09:51 +0100

    Create encrypt_and_tar.sh

---
+++ encrypt_and_tar.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# REFERENCE 
+#https://www.shellhacks.com/encrypt-decrypt-file-password-openssl/
+#https://www.tecmint.com/encrypt-decrypt-files-tar-openssl-linux/
+
+
+echo "hello word" > file_to_be_encrypted.txt
+
+# THE ARCHIVE WILL BE CREATED WITH ENCRYPTION (this is the defalt password 'PASS')
+# encrypt (tar -czf - file_to_be_encrypted.txt | openssl enc -e -aes256 -out secured.tar.gz -k PASS)
+# decrypt (openssl enc -d -aes256 -in secured.tar.gz -k PASS | tar xz -C .)
