9558695726269fbbc93ef4f9ae3b3c8325c82109
Caliari Jr <Calliari@users.noreply.github.com>
2019-06-27 19:52:06 +0100

    Update start-up-script.sh

--- start-up/start-up-script.sh
+++ start-up/start-up-script.sh
@@ -5,15 +5,15 @@
 
 sudo sed -i s/"exit 0"/""/g /etc/rc.local
 
-echo -e "\n# run every time this server is restarted \n/home/ubuntu/start-up.sh || exit 1 \n" | sudo tee -a /etc/rc.local
+echo -e "\n# run every time this server is restarted \n ~/start-up.sh || exit 1 \n" | sudo tee -a /etc/rc.local
 
 echo "exit 0" | sudo tee -a /etc/rc.local
 
-
+cd ~/
 
 #=============================================================================
 
-sudo tee <<'EOF'>> /home/ubuntu/start-up.sh
+sudo tee <<'EOF'>>  ~/start-up.sh
 #!/bin/bash
 
 echo pass1234 | passwd ec2-user --stdin
@@ -22,4 +22,4 @@ exit
 EOF
 
 #=============================================================================
-sudo chmod +x /home/ubuntu/start-up.sh
+sudo chmod +x  ~/start-up.sh
