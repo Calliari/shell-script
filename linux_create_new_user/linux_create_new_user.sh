45a632adbc0a237440f2a3bd57aa7dea17a8b351
Caliari Jr <Calliari@users.noreply.github.com>
2019-06-27 20:18:03 +0100

    Update linux_create_new_user.sh

--- linux_create_new_user/linux_create_new_user.sh
+++ linux_create_new_user/linux_create_new_user.sh
@@ -4,11 +4,14 @@
 # Script to add adn allow ssh access using passwd
 #
 
-
+# ubuntu & centos
 sudo sed -i s/"PasswordAuthentication no"/"PasswordAuthentication yes"/ /etc/ssh/sshd_config
+
+# centos
+sudo service sshd restart
+
+# ubuntu
 sudo service ssh restart
-
-
 
 # ========================================================
 # ADD a User on Ubuntu
@@ -29,3 +32,7 @@ echo "Passwd: paSSword123"
 # sudo deluser --remove-home newuser
 # sudo userdel newuser
 # sudo deluser newuser
+# ========================================================
+
+# test the ssh connection with username@server-ip E.X
+ssh newuser@1.2.3.4 # type the pwd
