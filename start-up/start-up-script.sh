638dad0544f0b347f1046d30de6a9da7eb6e7406
Caliari Jr <Calliari@users.noreply.github.com>
2019-06-27 21:33:29 +0100

    Update start-up-script.sh

--- start-up/start-up-script.sh
+++ start-up/start-up-script.sh
@@ -4,15 +4,24 @@
 
 echo $(whoami) | sudo tee /tmp/test.txt
 
+# ========================================================
+# ALLOW USER SSH WITH PWD (UBUNTU & CENTOS)
+sudo sed -i s/"PasswordAuthentication no"/"PasswordAuthentication yes"/ /etc/ssh/sshd_config
+
 #=============================================================================
+# UBUNTU
 # add the "trigger" for a script run at every reboot on this server
 
-sudo sed -i s/"exit 0"/""/g /etc/rc.local
-
-echo -e "\n# run every time this server is restarted \n ~/start-up.sh || exit 1 \n" | sudo tee -a /etc/rc.local
-
-echo "exit 0" | sudo tee -a /etc/rc.local
 sudo chmod +x /etc/rc.local
+sudo sed -i s/"exit 0"/""/g /etc/rc.local
+echo -e "\n# run every time this server is restarted \n ~/start-up.sh || exit 1 \n" | sudo tee -a /etc/rc.local
+echo "exit 0" | sudo tee -a /etc/rc.local
+
+# CENTOS
+sudo chmod +x /etc/rc.d/rc.local
+sudo sed -i s/"exit 0"/""/g /etc/rc.d/rc.local
+echo -e "\n# run every time this server is restarted \n ~/start-up.sh || exit 1 \n" | sudo tee -a /etc/rc.d/rc.local
+echo "exit 0" | sudo tee -a /etc/rc.d/rc.local
 
 #=============================================================================
 
@@ -24,15 +33,8 @@ sudo tee <<'EOF'>>  ~/start-up.sh
 # useradd -h
 sudo useradd -p $(openssl passwd -1 paSSword123) -s /bin/bash -m newuser
 
-# ========================================================
-# ALLOW USER SSH WITH PQD
-sudo sed -i s/"PasswordAuthentication no"/"PasswordAuthentication yes"/ /etc/ssh/sshd_config
-
-# centos
-sudo service sshd restart
-
-# ubuntu
-sudo service ssh restart
+#         CENTOS          or        UBUNTU 
+sudo service sshd restart || sudo service ssh restart
 
 
 exit
