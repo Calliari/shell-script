f69d414d80c4425501541e283eca34ea82fc89fc
Caliari Jr <Calliari@users.noreply.github.com>
2020-03-20 13:16:35 +0000

    Update encrypt_and_tar.sh

--- encrypt_and_tar.sh
+++ encrypt_and_tar.sh
@@ -10,3 +10,9 @@ echo "hello word" > file_to_be_encrypted.txt
 # THE ARCHIVE WILL BE CREATED WITH ENCRYPTION (this is the defalt password 'PASS')
 # encrypt (tar -czf - file_to_be_encrypted.txt | openssl enc -e -aes256 -out secured.tar.gz -k PASS)
 # decrypt (openssl enc -d -aes256 -in secured.tar.gz -k PASS | tar xz -C .)
+
+
+
+# NO ENCRYPTION (just normal conpress with tar)
+echo "hello word" > file_to_be_compressed.txt
+tar -zcvf file_to_be_compressed.txt file_to_be_compressed.txt.tar.gz
