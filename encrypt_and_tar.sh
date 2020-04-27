946142149fe1d78b30d48a6d2ef371643a93ef9b
Caliari Jr <Calliari@users.noreply.github.com>
2020-04-27 11:55:08 +0100

    Update encrypt_and_tar.sh

--- encrypt_and_tar.sh
+++ encrypt_and_tar.sh
@@ -3,6 +3,7 @@
 # REFERENCE 
 #https://www.shellhacks.com/encrypt-decrypt-file-password-openssl/
 #https://www.tecmint.com/encrypt-decrypt-files-tar-openssl-linux/
+#https://coderwall.com/p/tlec0g/openssl-encrypted-tar-backups-for-unix-like-s
 
 
 echo "hello word" > file_to_be_encrypted.txt
@@ -15,6 +16,13 @@ echo "hello word" > file_to_be_encrypted.txt
 # strong encription
 encrypt (cat secured.tar.gz | openssl enc -e -pbkdf2 -aes256 -out secured.tar.gz -k PASS)
 decrypt (openssl enc -d -pbkdf2 -aes256 -in secured.tar.gz -k PASS | tar xz -C .)
+
+# strong encription method 2
+#encrypt
+sudo tar czf - /dir_to_encript | openssl enc -e -aes-256-cbc -pbkdf2 -out /tmp/destination_dir_enc.$(date +"%Y-%m-%d").tgz -k PASSWORD-HERE
+#decrypt
+openssl enc -d -aes-256-cbc -pbkdf2 -in /tmp/destination_dir_enc.$(date +"%Y-%m-%d").tgz -k PASSWORD-HERE | tar zxf -
+
 
 # NO ENCRYPTION (just normal conpress with tar)
 echo "hello word" > file_to_be_compressed.txt
