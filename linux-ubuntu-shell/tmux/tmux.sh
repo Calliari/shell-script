73ebb5d7f1998a4c49c082246d311027ed5c9b19
Caliari Jr <Calliari@users.noreply.github.com>
2021-06-22 16:52:05 +0100

    Update tmux.sh

--- linux-ubuntu-shell/tmux/tmux.sh
+++ linux-ubuntu-shell/tmux/tmux.sh
@@ -1,2 +1,14 @@
-# install tmux on ubuntu 
+# Install tmux on ubuntu 
 sudo apt-get install tmux
+
+# All tmux commands begin with the prefix "^b" (control b) so enter the prefix following by.a command:
+
+# Split the panel horizontally
+```
+^b "
+```
+
+# Split the panel vertically
+```
+^b %
+```
