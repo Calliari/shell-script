53444ee4cdf790d4d0c35581cbe1a2cf79adda3e
Caliari Jr <Calliari@users.noreply.github.com>
2021-06-24 12:49:28 +0100

    Update bash_loop.sh

--- linux-ubuntu-shell/shell_loop/bash_loop.sh
+++ linux-ubuntu-shell/shell_loop/bash_loop.sh
@@ -1,5 +1,7 @@
 
+1) ###### ---- #######
 # One dimensional array in Bash
+
 NAMES=(
 "Test CAVALCANTE"
 "Jack Smith"
@@ -16,8 +18,7 @@ for NAME in "${NAMES[@]}" ; do
 done
 
 
-###### ---- #######
-
+2) ###### ---- #######
 # Multi-dimensional arrays in Bash (WITH NUMBER IN SEQUENCE)
 
 # Declare permit modifying the properties of variables (-a for arrays)
@@ -29,4 +30,29 @@ words=(one two three four five six seven)
 
 for var in ${num[@]}; do
     echo "In Number:${num[$var]} = In Numeral:${words[$var]}"
+done
+
+
+
+3) ###### ---- #######
+# Multi-dimensional arrays in Bash (WITH NUMBER IN SEQUENCE)
+
+#   all xml in one array
+xml=(
+    'script1'
+    'script2'
+    'script3'
+    'script4'
+)
+
+#   all context in one array
+context=(
+    'test1'
+    'test2'
+    'test3'
+    'test4'
+)
+
+for (( i = 1; i < ${#context[@]} ; i++ )); do
+    echo "xml-file: ${xml[$i]} with ${context[$i]}"
 done
