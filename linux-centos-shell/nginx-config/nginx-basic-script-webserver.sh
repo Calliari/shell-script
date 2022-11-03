0fd48b2bbedaa6af2a1fd3dbb40d16d330edf1ad
Caliari Jr <Calliari@users.noreply.github.com>
2022-11-03 08:54:51 +0000

    Create nginx-basic-script-webserver.sh

---
+++ linux-centos-shell/nginx-config/nginx-basic-script-webserver.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+#
+sudo yum update -y
+
+# OPEL - if centos core linux
+sudo yum install -y epel-release 
+# OPEL - if centos amazon linux 
+sudo amazon-linux-extras install -y epel
+
+sudo yum update -y 
+
+#
+sudo yum install -y nginx
+
+#
+sudo systemctl start nginx
+sudo systemctl enable nginx
+
+#
+sudo systemctl status nginx
