cd1212a7cb9c1a38703d8fdeb5262c86ce818f6d
Caliari Jr <Calliari@users.noreply.github.com>
2022-11-04 15:15:49 +0000

    Create nginx_test_curl_opensssl.sh

---
+++ linux-ubuntu-shell/nginx-config/nginx_test_curl_opensssl.sh
@@ -0,0 +1,33 @@
+# Testing request
+
+# send a request to the nginx sevrer with HOSTNAME from using the localhost
+curl --verbose --header 'Host: HOSTNAME WHERE THE NGINX IS CONFIGURED' -k 'https://127.0.0.1:8201'
+
+
+# Testing with the client certs to the endpoinht (http or https)
+curl --cacert ./ca.crt \. # The CA certificate belonging to the CA that signed the server's certificate (if it is not already included with your OS trusted certs)
+     --key ./client.key \ # Your client certificate
+     --cert ./client.crt \ # Your client private key
+     https:[EXAMPLE_URL]
+
+
+# Testing with the client certs to the endpoinht (http or https) using the localhost
+curl -v --cert ./certificate.crt --key ./key-certificate.key --header 'Host: example.com'  http://127.0.0.1:443
+curl -v --cert ./certificate.crt --key ./key-certificate.key --cacert cert-authority-certificate.crt --header 'Host: example.com'  http://127.0.0.1:443
+
+
+# Testing with the client certs to the endpoinht (http or https) using the localhost POST request with credentials
+curl -vv --location --request POST 'example.com/token' \
+--header 'Content-Type: application/x-www-form-urlencoded' \
+--data-urlencode 'grant_type=client_credentials' \
+--data-urlencode 'client_id=CLIENT-ID' \
+--data-urlencode 'client_secret=<CLIENT SECRET>'
+
+
+# Testing with openssl
+openssl s_client -connect localhost:443 -cert ./certificate.crt -key ./certificate-key.key -servername example.com 
+GET / HTTP/1.0 
+Host: example.com 
+
+# check the certificate used by the "servername"
+openssl s_client -connect localhost:443 -servername example.com   2>&1 | openssl x509 -text
