10da311996ffd9c69d9df00442082bf785b25a21
Caliari Jr <Calliari@users.noreply.github.com>
2022-11-11 17:09:18 +0000

    Update httpd-basic-script-webserver.sh

--- linux-centos-shell/http-config/httpd-basic-script-webserver.sh
+++ linux-centos-shell/http-config/httpd-basic-script-webserver.sh
@@ -4,12 +4,12 @@ sudo yum -y update httpd
 
 sudo yum -y install httpd
 
+sudo systemctl start httpd
+sudo systemctl enable httpd
+sudo systemctl status httpd
+
+
 # if firewalld is installed on the server it needs to open up port 80 to allow Apache to serve requests over HTTP and/or HTTPS
 sudo firewall-cmd --permanent --add-service=http
 sudo firewall-cmd --permanent --add-service=https
 sudo firewall-cmd --reload
-
-
-sudo systemctl start httpd
-sudo systemctl enable httpd
-sudo systemctl status httpd
